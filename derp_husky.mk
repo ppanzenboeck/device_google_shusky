787bcf5b9fa1f27d357d5b60b8eee63ca8dedee4
ppanzenboeck <philipp.panzenboeck1@gmail.com>
2024-08-07 00:29:02 +0200

    P.A.N.Z. Adapations (DerpFest)

--- derp_husky.mk
+++ derp_husky.mk
@@ -6,7 +6,7 @@
 
 # Inherit some common Lineage stuff.
 TARGET_DISABLE_EPPE := true
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/shusky/aosp_husky.mk)
@@ -17,7 +17,7 @@ include device/google/shusky/husky/device-lineage.mk
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 8 Pro
-PRODUCT_NAME := lineage_husky
+PRODUCT_NAME := derp_husky
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2992
